b83c8262429b3d1e0b5deb7653d0d58688ad6661
Matthew Reichardt <me@matthewreichardt.com>
2016-11-17 17:37:20 -0600

    Added responsiveness to new logo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+/* Authparty Added */
+$(window).scroll(function(){
+  var y = $(window).scrollTop();
+    if (y >= 500){
+      $(".logo").hide();
+			$(".logo-menu").show();
+    }
+    else {
+			$(".logo-menu").hide();
+      $(".logo").show();
+    }
+});
 
 (function($) {
 
